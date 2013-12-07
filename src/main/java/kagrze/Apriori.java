9c6ae6d169246ddbaee290f2aad2128ddd28dfd7
Karol Grzegorczyk <kagrze@gmail.com>
2013-12-07 18:57:24 +0100

    styles and ignores

--- src/main/java/kagrze/Apriori.java
+++ src/main/java/kagrze/Apriori.java
@@ -11,142 +11,127 @@ import com.google.common.collect.TreeMultiset;
 /**
  * Implementation of the Apriori algorithm introduced in the article 
  * "Fast algorithms for mining association rules" by R. Agrawal and R. Srikant
+ *
  * @author Karol Grzegorczyk
  *
  */
 public class Apriori {
 
-	/**
-	 * @param transactions It is assumed that items within a transaction are sorted in lexicographic order.
-	 * @param support
-	 * @return
-	 */
-	public <E extends Comparable> Multiset<List<E>> mineFrequentItemSets(List<List<E>> transactions, int support/*does support should be floating point?*/) {
-		Multiset<List<E>> frequentItemsets = genFrequent1Itemsets(transactions, support);
+    /**
+     * @param transactions It is assumed that items within a transaction are sorted in lexicographic order.
+     * @param support
+     * @return
+     */
+    public <E extends Comparable> Multiset<List<E>> mineFrequentItemSets(List<List<E>> transactions, int support/*does support should be floating point?*/) {
+        Multiset<List<E>> frequentItemsets = genFrequent1Itemsets(transactions, support);
 
-		List<List<E>> candidatesForFrequentKItemsets = aprioriGen(new ArrayList<>(frequentItemsets.elementSet()));
+        List<List<E>> candidatesForFrequentKItemsets = aprioriGen(new ArrayList<>(frequentItemsets.elementSet()));
 
-		for(int size=2; candidatesForFrequentKItemsets.size() > 0; size++) {
-			Multiset<List<E>> allKItemsets = getAllKItemsets(transactions, candidatesForFrequentKItemsets, size);
-			Multiset<List<E>> frequentKItemsets = getFrequentKItemsets(allKItemsets, support);
-			frequentItemsets.addAll(frequentKItemsets);
-			candidatesForFrequentKItemsets = aprioriGen(new ArrayList<>(frequentKItemsets.elementSet()));
-		}
-		
-		return frequentItemsets;
-	}
-	
-	/**
-	 * 
-	 * @param transactions
-	 * @param support
-	 * @param confidence
-	 */
-	public <E extends Comparable> List<E> induceRules(List<List<E>> transactions, int support, float minConfidence) {
-		Multiset<List<E>> frequentItemsets = mineFrequentItemSets(transactions, support);
-		return genRules(frequentItemsets.iterator().next(),-1,null,-1,minConfidence);
-	}
+        for(int size=2; candidatesForFrequentKItemsets.size() > 0; size++) {
+            Multiset<List<E>> allKItemsets = getAllKItemsets(transactions, candidatesForFrequentKItemsets, size);
+            Multiset<List<E>> frequentKItemsets = getFrequentKItemsets(allKItemsets, support);
+            frequentItemsets.addAll(frequentKItemsets);
+            candidatesForFrequentKItemsets = aprioriGen(new ArrayList<>(frequentKItemsets.elementSet()));
+        }
+        
+        return frequentItemsets;
+    }
 
-	<E> List<E> genRules(List<E> lk, int lkSupport, List<E> am, int amSupport, float minConfidence) {
-//		float confidence = 
-		return null;
-	}
-	
-	private <E extends Comparable> Multiset<List<E>> genFrequent1Itemsets(List<List<E>> transactions, int support) {
-		SortedMultiset<E> all1Itemsets = TreeMultiset.create();
-		for (List<E> transaction : transactions)
-			for (E item : transaction)
-				all1Itemsets.add(item);
-		Multiset<List<E>> frequent1Itemsets = LinkedHashMultiset.create();
-		for (E item : all1Itemsets.elementSet()) {
-			int count = all1Itemsets.count(item);
-			if(count >= support) {
-				List<E> itemset = new ArrayList<>();
-				itemset.add(item);
-				frequent1Itemsets.add(itemset,count);
-			}
-		}
-		return frequent1Itemsets;
-	}
+    private <E extends Comparable> Multiset<List<E>> genFrequent1Itemsets(List<List<E>> transactions, int support) {
+        SortedMultiset<E> all1Itemsets = TreeMultiset.create();
+        for (List<E> transaction : transactions)
+            for (E item : transaction)
+                all1Itemsets.add(item);
+        Multiset<List<E>> frequent1Itemsets = LinkedHashMultiset.create();
+        for (E item : all1Itemsets.elementSet()) {
+            int count = all1Itemsets.count(item);
+            if(count >= support) {
+                List<E> itemset = new ArrayList<>();
+                itemset.add(item);
+                frequent1Itemsets.add(itemset,count);
+            }
+        }
+        return frequent1Itemsets;
+    }
 
-	private <E> Multiset<List<E>> getAllKItemsets(
-			List<List<E>> transactions, List<List<E>> candidatesForFrequentKItemsets, int size) {
-		Multiset<List<E>> allKItemsets = LinkedHashMultiset.create();
-		for (List<E> transaction : transactions) {
-			List<List<E>> combinations = generateAllCombinationsWithoutRepetitions(transaction, size);
-			for (List<E> candidateForFrequentKItemset : candidatesForFrequentKItemsets) {
-				for (List<E> combination : combinations) {
-					if (combination.equals(candidateForFrequentKItemset)) {
-						allKItemsets.add(candidateForFrequentKItemset);
-						break;
-					}
-				}
-			}
-		}
-		return allKItemsets;
-	}
+    private <E> Multiset<List<E>> getAllKItemsets(
+            List<List<E>> transactions, List<List<E>> candidatesForFrequentKItemsets, int size) {
+        Multiset<List<E>> allKItemsets = LinkedHashMultiset.create();
+        for (List<E> transaction : transactions) {
+            List<List<E>> combinations = generateAllCombinationsWithoutRepetitions(transaction, size);
+            for (List<E> candidateForFrequentKItemset : candidatesForFrequentKItemsets) {
+                for (List<E> combination : combinations) {
+                    if (combination.equals(candidateForFrequentKItemset)) {
+                        allKItemsets.add(candidateForFrequentKItemset);
+                        break;
+                    }
+                }
+            }
+        }
+        return allKItemsets;
+    }
 
-	private <E> Multiset<List<E>> getFrequentKItemsets( Multiset<List<E>> allKItemsets, int support) {
-		Multiset<List<E>> frequentKItemsets = LinkedHashMultiset.create();
-		for (List<E> itemset : allKItemsets.elementSet()) {
-			int count = allKItemsets.count(itemset);
-			if (count >= support)
-				frequentKItemsets.add(itemset,count);
-		}
-		return frequentKItemsets;
-	}
-	
-	/**
-	 * This function contains two steps: join step and prune step
-	 * @param frequentItemsets Items within frequent K-1 itemsets have to be lexicographically ordered!
-	 * @return
-	 */
-	<E> List<List<E>> aprioriGen(List<List<E>> frequentItemsets) {
-		List<List<E>> candidates = new ArrayList<>();
+    private <E> Multiset<List<E>> getFrequentKItemsets( Multiset<List<E>> allKItemsets, int support) {
+        Multiset<List<E>> frequentKItemsets = LinkedHashMultiset.create();
+        for (List<E> itemset : allKItemsets.elementSet()) {
+            int count = allKItemsets.count(itemset);
+            if (count >= support)
+                frequentKItemsets.add(itemset,count);
+        }
+        return frequentKItemsets;
+    }
+    
+    /**
+     * This function contains two steps: join step and prune step
+     * @param frequentItemsets Items within frequent K-1 itemsets have to be lexicographically ordered!
+     * @return
+     */
+    <E> List<List<E>> aprioriGen(List<List<E>> frequentItemsets) {
+        List<List<E>> candidates = new ArrayList<>();
 
-		for (int i=0; i<frequentItemsets.size(); i++) {
-			for (int j=i+1; j<frequentItemsets.size(); j++) {
-				List<E> lhs = frequentItemsets.get(i);
-				List<E> rhs = frequentItemsets.get(j);
-				if (lhs.size()==1 || lhs.subList(0, lhs.size()-1).equals(rhs.subList(0,rhs.size()-1))) {
-					List<E> candidate = new ArrayList<>(lhs);
-					//join
-					candidate.add(rhs.get(rhs.size()-1));
-					//prune (based on the Apriori property)
-					if(!hasInfrequentSubset(candidate,frequentItemsets))
-						candidates.add(candidate);
-				}
-			}
-		}
-		return candidates;
-	}
-	
-	private <E> boolean hasInfrequentSubset(List<E> candidate, List<List<E>> frequentItemsets) {
-		for (List<E> subset : generateAllCombinationsWithoutRepetitions(candidate,candidate.size()-1)) {
-			if (!frequentItemsets.contains(subset)) {
-				return true;
-			}
-		}
-		return false;
-	}
+        for (int i=0; i<frequentItemsets.size(); i++) {
+            for (int j=i+1; j<frequentItemsets.size(); j++) {
+                List<E> lhs = frequentItemsets.get(i);
+                List<E> rhs = frequentItemsets.get(j);
+                if (lhs.size()==1 || lhs.subList(0, lhs.size()-1).equals(rhs.subList(0,rhs.size()-1))) {
+                    List<E> candidate = new ArrayList<>(lhs);
+                    //join
+                    candidate.add(rhs.get(rhs.size()-1));
+                    //prune (based on the Apriori property)
+                    if(!hasInfrequentSubset(candidate,frequentItemsets))
+                        candidates.add(candidate);
+                }
+            }
+        }
+        return candidates;
+    }
+    
+    private <E> boolean hasInfrequentSubset(List<E> candidate, List<List<E>> frequentItemsets) {
+        for (List<E> subset : generateAllCombinationsWithoutRepetitions(candidate,candidate.size()-1)) {
+            if (!frequentItemsets.contains(subset)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	<E> List<List<E>> generateAllCombinationsWithoutRepetitions(List<E> itemset, int subsetSize) {
-		List<List<E>> combinations = new ArrayList<>();
-		List<E> combinationPart = new ArrayList<>();
-		combinationsRecurrent(itemset,subsetSize,0,0,combinationPart,combinations);
-		return combinations;
-	}
-	
-	private <E> void combinationsRecurrent(List<E> itemset, int subsetSize, int level, int index, 
-			List<E> combinationPart, List<List<E>> combinations) {
-		if (level == subsetSize) {
-			combinations.add(combinationPart);
-		} else {
-			for(int i = index; i<=itemset.size()-subsetSize+level; i++) {
-				List<E> newCombinationPart = new ArrayList<>(combinationPart);
-				newCombinationPart.add(itemset.get(i));
-				combinationsRecurrent(itemset, subsetSize, level+1, i+1, newCombinationPart, combinations);
-			}
-		}
-	}
+    <E> List<List<E>> generateAllCombinationsWithoutRepetitions(List<E> itemset, int subsetSize) {
+        List<List<E>> combinations = new ArrayList<>();
+        List<E> combinationPart = new ArrayList<>();
+        combinationsRecurrent(itemset,subsetSize,0,0,combinationPart,combinations);
+        return combinations;
+    }
+    
+    private <E> void combinationsRecurrent(List<E> itemset, int subsetSize, int level, int index, 
+            List<E> combinationPart, List<List<E>> combinations) {
+        if (level == subsetSize) {
+            combinations.add(combinationPart);
+        } else {
+            for(int i = index; i<=itemset.size()-subsetSize+level; i++) {
+                List<E> newCombinationPart = new ArrayList<>(combinationPart);
+                newCombinationPart.add(itemset.get(i));
+                combinationsRecurrent(itemset, subsetSize, level+1, i+1, newCombinationPart, combinations);
+            }
+        }
+    }
 }
--- src/test/java/kagrze/AprioriTest.java
+++ src/test/java/kagrze/AprioriTest.java
@@ -18,126 +18,117 @@ import com.google.common.collect.Multiset;
  */
 public class AprioriTest {
 
-	@Test
-	public void testMineFrequentItemSets() {
-		List<List<String>> database = new ArrayList<>();
-		database.add(createItemset("A","C"));
-		database.add(createItemset("A","B","C","D"));
-		database.add(createItemset("A","C","D"));
-		Multiset<List<String>> frequentItemsets = new Apriori().mineFrequentItemSets(database, 3);
-		
-		Multiset<List<String>> expectedItemsets = LinkedHashMultiset.create();
-		expectedItemsets.add(createItemset("A"),3);
-		expectedItemsets.add(createItemset("C"),3);
-		expectedItemsets.add(createItemset("A","C"),3);
-		assertEquals(expectedItemsets, frequentItemsets);
-	}
-	
-	@Test
-	public void testMineFrequentItemSets2() {
-		List<List<String>> database = new ArrayList<>();
-		database.add(createItemset("A","C"));
-		database.add(createItemset("A","D"));
-		database.add(createItemset("A","B","C"));
-		database.add(createItemset("A","C","D"));
-		Multiset<List<String>> frequentItemsets = new Apriori().mineFrequentItemSets(database, 2);
-		
-		Multiset<List<String>> expectedItemsets = LinkedHashMultiset.create();
-		expectedItemsets.add(createItemset("A"),4);
-		expectedItemsets.add(createItemset("C"),3);
-		expectedItemsets.add(createItemset("D"),2);
-		expectedItemsets.add(createItemset("A","C"),3);
-		expectedItemsets.add(createItemset("A","D"),2);
-		assertEquals(expectedItemsets, frequentItemsets);
-	}
-	
-	@Test
-	public void testMineFrequentItemSets3() {
-		List<List<String>> database = new ArrayList<>();
-		database.add(createItemset("A","B","C","D","E"));
-		database.add(createItemset("B","C","D","E","F"));
-		database.add(createItemset("C","D","E","F","G"));
-		Multiset<List<String>> frequentItemsets = new Apriori().mineFrequentItemSets(database, 3);
-		
-		Multiset<List<String>> expectedItemsets = LinkedHashMultiset.create();
-		expectedItemsets.add(createItemset("C"),3);
-		expectedItemsets.add(createItemset("D"),3);
-		expectedItemsets.add(createItemset("E"),3);
-		expectedItemsets.add(createItemset("C","D"),3);
-		expectedItemsets.add(createItemset("C","E"),3);
-		expectedItemsets.add(createItemset("D","E"),3);
-		expectedItemsets.add(createItemset("C","D","E"),3);
-		assertEquals(expectedItemsets, frequentItemsets);
-	}
-	
-	@Test
-	public void testJoin() {
-		List<List<String>> frequent1Itemsets = new ArrayList<>();
-		frequent1Itemsets.add(createItemset("A"));
-		frequent1Itemsets.add(createItemset("B"));
-		frequent1Itemsets.add(createItemset("C"));
-		frequent1Itemsets.add(createItemset("D"));
-		List<List<String>> candidates = new Apriori().aprioriGen(frequent1Itemsets);
-		List<List<String>> expectedCandidates = new ArrayList<>();
-		expectedCandidates.add(createItemset("A", "B"));
-		expectedCandidates.add(createItemset("A", "C"));
-		expectedCandidates.add(createItemset("A", "D"));
-		expectedCandidates.add(createItemset("B", "C"));
-		expectedCandidates.add(createItemset("B", "D"));
-		expectedCandidates.add(createItemset("C", "D"));
-		assertEquals(expectedCandidates, candidates);
-	}
-	
-	@Test
-	public void testJoin2() {
-		List<List<Integer>> frequent3Itemsets = new ArrayList<>();
-		frequent3Itemsets.add(createItemset(1,2,3));
-		frequent3Itemsets.add(createItemset(1,2,4));
-		frequent3Itemsets.add(createItemset(1,3,4));
-		frequent3Itemsets.add(createItemset(1,3,5));
-		frequent3Itemsets.add(createItemset(2,3,4));
-		List<List<Integer>> candidates = new Apriori().aprioriGen(frequent3Itemsets);
-		List<List<Integer>> expectedCandidates = new ArrayList<>();
-		expectedCandidates.add(createItemset(1,2,3,4));
-		assertEquals(expectedCandidates, candidates);
-	}
-	
-	@Test
-	public void testGenerateAllCombinationsWithoutRepetitions() {
-		List<String> itemset = new ArrayList<>();
-		itemset.add("A");
-		itemset.add("B");
-		itemset.add("C");
-		itemset.add("D");
-		itemset.add("E");
-		List<List<String>> combinations = new Apriori().generateAllCombinationsWithoutRepetitions(itemset,3);
-		List<List<String>> expectedCombinations = new ArrayList<>();
-		expectedCombinations.add(createItemset("A","B","C"));
-		expectedCombinations.add(createItemset("A","B","D"));
-		expectedCombinations.add(createItemset("A","B","E"));
-		expectedCombinations.add(createItemset("A","C","D"));
-		expectedCombinations.add(createItemset("A","C","E"));
-		expectedCombinations.add(createItemset("A","D","E"));
-		expectedCombinations.add(createItemset("B","C","D"));
-		expectedCombinations.add(createItemset("B","C","E"));
-		expectedCombinations.add(createItemset("B","D","E"));
-		expectedCombinations.add(createItemset("C","D","E"));
-		assertEquals(expectedCombinations, combinations);
-	}
-
-	@Test
-	public void testInduceRules() {
-		List<List<String>> database = new ArrayList<>();
-		database.add(createItemset("A","C"));
-		database.add(createItemset("A","B","C","D"));
-		database.add(createItemset("A","C","D"));
-		new Apriori().induceRules(database, 3, 2);
-	}
-	
-	private <E> List<E> createItemset(E ... items) {
-		List<E> itemset = new ArrayList<>();
-		for (E item : items)
-			itemset.add(item);
-		return itemset;
-	}
+    @Test
+    public void testMineFrequentItemSets() {
+        List<List<String>> database = new ArrayList<>();
+        database.add(createItemset("A","C"));
+        database.add(createItemset("A","B","C","D"));
+        database.add(createItemset("A","C","D"));
+        Multiset<List<String>> frequentItemsets = new Apriori().mineFrequentItemSets(database, 3);
+        
+        Multiset<List<String>> expectedItemsets = LinkedHashMultiset.create();
+        expectedItemsets.add(createItemset("A"),3);
+        expectedItemsets.add(createItemset("C"),3);
+        expectedItemsets.add(createItemset("A","C"),3);
+        assertEquals(expectedItemsets, frequentItemsets);
+    }
+    
+    @Test
+    public void testMineFrequentItemSets2() {
+        List<List<String>> database = new ArrayList<>();
+        database.add(createItemset("A","C"));
+        database.add(createItemset("A","D"));
+        database.add(createItemset("A","B","C"));
+        database.add(createItemset("A","C","D"));
+        Multiset<List<String>> frequentItemsets = new Apriori().mineFrequentItemSets(database, 2);
+        
+        Multiset<List<String>> expectedItemsets = LinkedHashMultiset.create();
+        expectedItemsets.add(createItemset("A"),4);
+        expectedItemsets.add(createItemset("C"),3);
+        expectedItemsets.add(createItemset("D"),2);
+        expectedItemsets.add(createItemset("A","C"),3);
+        expectedItemsets.add(createItemset("A","D"),2);
+        assertEquals(expectedItemsets, frequentItemsets);
+    }
+    
+    @Test
+    public void testMineFrequentItemSets3() {
+        List<List<String>> database = new ArrayList<>();
+        database.add(createItemset("A","B","C","D","E"));
+        database.add(createItemset("B","C","D","E","F"));
+        database.add(createItemset("C","D","E","F","G"));
+        Multiset<List<String>> frequentItemsets = new Apriori().mineFrequentItemSets(database, 3);
+        
+        Multiset<List<String>> expectedItemsets = LinkedHashMultiset.create();
+        expectedItemsets.add(createItemset("C"),3);
+        expectedItemsets.add(createItemset("D"),3);
+        expectedItemsets.add(createItemset("E"),3);
+        expectedItemsets.add(createItemset("C","D"),3);
+        expectedItemsets.add(createItemset("C","E"),3);
+        expectedItemsets.add(createItemset("D","E"),3);
+        expectedItemsets.add(createItemset("C","D","E"),3);
+        assertEquals(expectedItemsets, frequentItemsets);
+    }
+    
+    @Test
+    public void testJoin() {
+        List<List<String>> frequent1Itemsets = new ArrayList<>();
+        frequent1Itemsets.add(createItemset("A"));
+        frequent1Itemsets.add(createItemset("B"));
+        frequent1Itemsets.add(createItemset("C"));
+        frequent1Itemsets.add(createItemset("D"));
+        List<List<String>> candidates = new Apriori().aprioriGen(frequent1Itemsets);
+        List<List<String>> expectedCandidates = new ArrayList<>();
+        expectedCandidates.add(createItemset("A", "B"));
+        expectedCandidates.add(createItemset("A", "C"));
+        expectedCandidates.add(createItemset("A", "D"));
+        expectedCandidates.add(createItemset("B", "C"));
+        expectedCandidates.add(createItemset("B", "D"));
+        expectedCandidates.add(createItemset("C", "D"));
+        assertEquals(expectedCandidates, candidates);
+    }
+    
+    @Test
+    public void testJoin2() {
+        List<List<Integer>> frequent3Itemsets = new ArrayList<>();
+        frequent3Itemsets.add(createItemset(1,2,3));
+        frequent3Itemsets.add(createItemset(1,2,4));
+        frequent3Itemsets.add(createItemset(1,3,4));
+        frequent3Itemsets.add(createItemset(1,3,5));
+        frequent3Itemsets.add(createItemset(2,3,4));
+        List<List<Integer>> candidates = new Apriori().aprioriGen(frequent3Itemsets);
+        List<List<Integer>> expectedCandidates = new ArrayList<>();
+        expectedCandidates.add(createItemset(1,2,3,4));
+        assertEquals(expectedCandidates, candidates);
+    }
+    
+    @Test
+    public void testGenerateAllCombinationsWithoutRepetitions() {
+        List<String> itemset = new ArrayList<>();
+        itemset.add("A");
+        itemset.add("B");
+        itemset.add("C");
+        itemset.add("D");
+        itemset.add("E");
+        List<List<String>> combinations = new Apriori().generateAllCombinationsWithoutRepetitions(itemset,3);
+        List<List<String>> expectedCombinations = new ArrayList<>();
+        expectedCombinations.add(createItemset("A","B","C"));
+        expectedCombinations.add(createItemset("A","B","D"));
+        expectedCombinations.add(createItemset("A","B","E"));
+        expectedCombinations.add(createItemset("A","C","D"));
+        expectedCombinations.add(createItemset("A","C","E"));
+        expectedCombinations.add(createItemset("A","D","E"));
+        expectedCombinations.add(createItemset("B","C","D"));
+        expectedCombinations.add(createItemset("B","C","E"));
+        expectedCombinations.add(createItemset("B","D","E"));
+        expectedCombinations.add(createItemset("C","D","E"));
+        assertEquals(expectedCombinations, combinations);
+    }
+    
+    private <E> List<E> createItemset(E ... items) {
+        List<E> itemset = new ArrayList<>();
+        for (E item : items)
+            itemset.add(item);
+        return itemset;
+    }
 }
